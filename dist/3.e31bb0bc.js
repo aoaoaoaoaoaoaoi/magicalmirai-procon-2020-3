4b9c183701a69dd763f1560296a05f2d9c7929be
unknown <gummi.bear456aaaaa@gmail.com>
2020-10-19 23:49:09 +0900

    アニメーションを複数適用

--- dist/3.e31bb0bc.js
+++ dist/3.e31bb0bc.js
@@ -292,7 +292,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49646" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57259" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
